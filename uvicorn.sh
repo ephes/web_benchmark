514fd70cc4ade665905646975a396dc03f4beea3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-09-28 18:06:25 +0200

    fixed arguments

--- uvicorn.sh
+++ uvicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 uvicorn_path=$(find /home/benchmark/.cache/pypoetry/virtualenvs -name "uvicorn" | grep -v lib)
-${uvicorn_path} -b :9000 --workers 8 benchmark.asgi:application
+${uvicorn_path} --port 9000 --host 0.0.0.0 --workers 8 benchmark.asgi:application
