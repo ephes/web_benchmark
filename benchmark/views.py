2408db851a819bda13a8a72f60316d9fccf9135b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-09-28 18:22:36 +0200

    just return text.. maybe reduce cpu hogging?

--- benchmark/views.py
+++ benchmark/views.py
@@ -1,5 +1,6 @@
 import time
 
+from django.http import HttpResponse
 from django.http import JsonResponse
 
 
@@ -12,7 +13,8 @@ def api(request):
 
 
 async def async_api(request):
-    payload = {"message": "Hello World!"}
-    if "task_id" in request.GET:
-        payload["task_id"] = request.GET["task_id"]
-    return JsonResponse(payload)
+    # payload = {"message": "Hello World!"}
+    # if "task_id" in request.GET:
+    #    payload["task_id"] = request.GET["task_id"]
+    # return JsonResponse(payload)
+    return HttpResponse('{"message": "Hello World!"}')
